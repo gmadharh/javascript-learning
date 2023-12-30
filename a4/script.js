b69d743c54a1463db9198e30a8d8eb69e9533ec4
Gevin <gevinmadharha@gmail.com>
2023-12-30 13:20:07 -0500

    adding styling changes

--- a4/script.js
+++ a4/script.js
@@ -5,10 +5,29 @@ console.log(counter)
 
 btn.forEach((element) => {
   element.addEventListener('click', (ele) => {
+    // check if clicking lower count button
     if (ele.target.innerHTML === 'LOWER COUNT') {
       counter.innerHTML = Number(counter.innerHTML) - 1
-    } else {
+    }
+
+    // else clicking add count button
+    else {
       counter.innerHTML = Number(counter.innerHTML) + 1
+    }
+
+    // if number less than zero, set to red
+    if (Number(counter.innerHTML) < 0) {
+      counter.style.color = 'red'
+    }
+
+    // else if number greater than zero, set to green
+    else if (Number(counter.innerHTML) > 0) {
+      counter.style.color = 'green'
+    }
+
+    // else its 0, set to black
+    else {
+      counter.style.color = 'black'
     }
   })
 })
