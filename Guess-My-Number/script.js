a5589787f3eb82d4db60252bd2182a7aa2f27005
Gevin <gevinmadharha@gmail.com>
2024-01-02 15:21:31 -0500

    working on implementing logic for guessing number

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,1 +1,15 @@
-'use strict';
+'use strict'
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1
+
+document.querySelector('.number').textContent = secretNumber
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value)
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!'
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉Correct Number!'
+  }
+})
