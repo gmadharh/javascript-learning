7638a9b586d84d44f70419b9d1a38314068e2fa9
Gevin <gevinmadharha@gmail.com>
2024-01-02 15:54:01 -0500

    finished logic of game, refactoring to avoid duplication

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,15 +1,63 @@
 'use strict'
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1
+let secretNumber = Math.trunc(Math.random() * 20) + 1
 
-document.querySelector('.number').textContent = secretNumber
+let score = 20
+let highScore = 0
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value)
 
+  // When there is no guess
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!'
-  } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉Correct Number!'
   }
+  // When player wins
+  else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉Correct Number!'
+
+    document.querySelector('.number').textContent = secretNumber
+
+    document.querySelector('body').style.backgroundColor = '#60b347'
+
+    document.querySelector('.number').style.width = '30rem'
+
+    if (score > highScore) {
+      highScore = score
+
+      document.querySelector('.highscore').textContent = highScore
+    }
+  }
+
+  // When guess is wrong
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? '📈Too high!' : '📉Too low!'
+      score -= 1
+      document.querySelector('.score').textContent = score
+    } else {
+      document.querySelector('.message').textContent = '💥You lost the game!'
+      document.querySelector('.score').textContent = 0
+    }
+  }
+})
+
+// Select play again button and add event listener
+document.querySelector('.again').addEventListener('click', () => {
+  // reset the score back to 20
+  score = 20
+  secretNumber = Math.trunc(Math.random() * 20) + 1
+
+  document.querySelector('.message').textContent = 'Start guessing...'
+
+  document.querySelector('.score').textContent = score
+
+  document.querySelector('.guess').value = ''
+
+  document.querySelector('body').style.backgroundColor = '#222'
+
+  document.querySelector('.number').style.width = '15rem'
+
+  document.querySelector('.number').textContent = '?'
 })
