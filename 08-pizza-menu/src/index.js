95ca3419f866149dc168b2a13314ccab489188ea
Gevin <gevinmadharha@gmail.com>
2024-01-17 10:17:15 -0500

    props

--- 08-pizza-menu/src/index.js
+++ 08-pizza-menu/src/index.js
@@ -57,16 +57,6 @@ function App() {
   )
 }
 
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci"></img>
-      <h3>Pizza Salamino</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  )
-}
-
 function Header() {
   return (
     <header className="header">
@@ -79,13 +69,35 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      />
     </main>
   )
 }
 
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name}></img>
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
+  )
+}
 function Footer() {
   const hour = new Date().getHours()
   const openHour = 8
